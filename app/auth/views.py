ae20e4d85ab16637ada0ea3f4c7f66b7a0b78e55
And770 <fengxipanwan@qq.com>
2017-12-08 20:43:28 +0800

    flasky-8.4.5

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,44 @@
+<< << << < Updated
+upstream
 from flask import render_template
 from . import  auth
 
 @auth.rout('/login')
 def log():
     return render_template('auth/login.html')
+
+== == == =
+from flask import render_template, redirect, flash, request, url_for
+from . import auth
+from flask_login import login_required, login_user, logout_user
+from .forms import LoginForm
+from ..models import User
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and User.verify_password(form.password.data):
+            login_user(user, form.remenber_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password!')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
+
+>> >> >> > Stashed
+changes
--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
 from . import db
+<< << << < Updated
+upstream
 from werkzeug.security import generate_password_hash, check_password_hash
+== == == =
+from . import login_manager
+
+>> >> >> > Stashed
+changes
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -11,9 +18,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
