d1c1f26f5cd46a78634fcb9a1e290be18637d4ce
And770 <fengxipanwan@qq.com>
2017-12-11 10:45:03 +0800

    flasky-9(finished)

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+
+def inject_permissions():
+    return dict(Permission=Permission)
